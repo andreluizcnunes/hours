ce49ba766194fd520eb2bd359dca68e39b721c48
André Luiz <andre.doctors@gmail.com>
2022-04-22 21:04:51 -0300

    ajustando js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,15 @@ function carregar(){
 
   if (hora >= 0 && hora < 12) {
     msg.innerHTML = `Bom dia! Agora são ${hora} horas`;
-    img.src = '/exercicio14/assets/img/morning.png';
+    img.src = '../img/morning.png';
     document.body.style.background = '#e2cd9f';
   } else if (hora >= 12 && hora < 18) {
     msg.innerHTML = `Boa Tarde! Agora são ${hora} horas`;
-    img.src = '/exercicio14/assets/img/evening.png';
+    img.src = '../img/evening.png';
     document.body.style.background = '#b9846f'
   } else {
     msg.innerHTML = `Boa Noite! Agora são ${hora} horas`;
-    img.src = '/exercicio14/assets/img/night.png';
+    img.src = '../img/night.png';
     document.body.style.background = '#515154';
   }
 }
